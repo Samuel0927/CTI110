e5c380207c607eeb91af760972705817e5f04a28
Samuel0927 <160054005+Samuel0927@users.noreply.github.com>
2024-03-25 14:26:22 -0400

    Add files via upload

---
+++ P1LAB1_RodriguezSamuel.py
@@ -0,0 +1,8 @@
+ # Samuel Rodriguez
+ # 03/25/24
+ # P1LAB1
+ # Using input and output
+
+firstname = input("Enter your firstname: ")
+lastname = input("Enter your lastname: ")
+print("Hello,", firstname, lastname, "! Welcome to CTI-110")
